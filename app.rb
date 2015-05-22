4320d61a060898bf895097f3032ff03a445ebc4a
daniel rendon <daniel.rendonfc@gmail.com>
2015-05-22 17:33:42 -0500

    done 2

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require "csv"
 
 get '/' do
+	
+	test = File.read("shouts.csv")
+	 @csv = CSV.parse(test) 
+	
 
-  erb :index
+
+	erb :index
 end
+
+post '/newshouts' do
+	@shouts = params[:shouts]
+	CSV.open('shouts.csv', 'ab') do |csv|
+		csv << [@shouts]
+	end	
+
+	redirect '/'
+	
+end	
